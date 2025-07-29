cc7d4b896cd9f70bcb8ac811cc9bc0f3067d23f5
sammymureithi20 <sammymureithi20.com>
2025-07-29 12:12:44 +0300

    personal referal code

--- app/Repositories/Auth/AuthRepository.php
+++ app/Repositories/Auth/AuthRepository.php
@@ -178,6 +178,9 @@ class AuthRepository implements AuthRepositoryInterface
                 'created_at' => $user->created_at,
                 'updated_at' => $user->updated_at,
                 'deleted_at' => $user->deleted_at,
+
+                'my_code' => $user->my_code,
+            
             
             
                 'role' => [
